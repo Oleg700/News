8e6157ebbd4667be0c24acd9bf2682e1a6f815a7
Oleg Aliyev <oleg_aliyev@epam.com>
2019-10-15 15:02:00 +0600

    added tomcat server

--- NewsManagement/src/main/java/com/epam/news/application/Application.java
+++ NewsManagement/src/main/java/com/epam/news/application/Application.java
@@ -2,10 +2,14 @@ package com.epam.news.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        /*NewsDao newsDao = new NewsDaoImpl();
+        newsDao.getAll();*/
     }
 }
